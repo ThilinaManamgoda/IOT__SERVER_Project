86fb5400a02f8287f5793ff19fc41d7060f0a523
Thilina Manamgoda <maanafunedu@gmail.com>
2015-11-21 12:04:47 +0530

    Testing

--- src/main/java/org/maana/iot_project/models/ServerConstants.java
+++ src/main/java/org/maana/iot_project/models/ServerConstants.java
@@ -5,5 +5,5 @@ public class ServerConstants {
 	public final static String URL_FOR_DEVICES ="/{userId}/devices";
 	public final static String URL_FOR_USERS ="/user";
 	public final static String URL_FOR_USERS_GET_OR_DELETE ="/{userId}";
-	public final static String TEST ="OK";
+	public final static String TEST ="OK ";
 }
